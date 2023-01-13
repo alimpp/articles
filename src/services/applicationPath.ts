471488995ef87cb619cedf6eb5397e1c04444d0c
Ali mohsenPour <86446834+alimpp@users.noreply.github.com>
2023-01-13 13:46:51 -0800

    Add new path in app paths

--- src/services/applicationPath.ts
+++ src/services/applicationPath.ts
@@ -1,9 +1,13 @@
-export const application_base_url: string =
-  "https://jsonplaceholder.typicode.com";
+export const application_base_url: string = "https://api.realworld.io";
 
 export const application_path = {
-  GET: {},
-  POST: {},
+  GET: {
+    ARTICLES:"/api/articles",
+  },
+  POST: {
+    LOGIN:"/api/users/login",
+    REGISTER:"/api/users"
+  },
   PUT: {},
   DELETE: {},
 };
